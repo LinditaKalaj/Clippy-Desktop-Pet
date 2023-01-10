b961987755f088ecbcba160d202741d378374ba0
lindita <linditakalaj@gmail.com>
2023-01-10 02:07:03 -0500

    reformat and added comments

--- clippy.py
+++ clippy.py
@@ -3,23 +3,27 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
-class TaskBar(QWidget):
+class ClippyInit(QWidget):
     def __init__(self):
-        super(TaskBar, self).__init__()
-        quit_clippy = QAction("Quit", self, triggered=self.close_program)
-        quit_clippy.setIcon(QIcon("img/exit_icon.png"))
+        super(ClippyInit, self).__init__()
+        self.setUpTrayIcon()
+
+    # Sets up and shows the desktop-pets tray
+    def setUpTrayIcon(self):
+        # Quit option on menu
+        self.quit_clippy = QAction("Quit", self,
+                                   triggered=QApplication.instance().quit)
+        self.quit_clippy.setIcon(QIcon("img/exit_icon.png"))
         self.trayIconMenu = QMenu(self)
-        self.trayIconMenu.addAction(quit_clippy)
+        self.trayIconMenu.addAction(self.quit_clippy)
+        # Tray
         self.trayIcon = QSystemTrayIcon(self)
         self.trayIcon.setIcon(QIcon("img/task_icon.png"))
         self.trayIcon.setContextMenu(self.trayIconMenu)
         self.trayIcon.show()
 
-    def close_program(self):
-        sys.exit()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    x = TaskBar()
-    app.exec_()
+    clippy = ClippyInit()
+    sys.exit(app.exec_())
