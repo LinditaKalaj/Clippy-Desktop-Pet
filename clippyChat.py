db6f19f078a0d1bc09b230cdf387ad3c04b8d33a
lindita <linditakalaj@gmail.com>
2023-01-11 21:08:04 -0500

    added autofocus

--- clippyChat.py
+++ clippyChat.py
@@ -32,13 +32,15 @@ class ClippyChat(QWidget):
         self.chatBox = QPlainTextEdit(self)
         self.chatBox.setReadOnly(True)
         self.inputTxt = QLineEdit(self)
+        self.inputTxt.setFocus(True)
         self.inputTxt.returnPressed.connect(self.sendToChatGPT)
-        self.inputTxt.setPlaceholderText("Ask a question or use the mic")
+        self.inputTxt.setPlaceholderText("Ask a question!")
         self.sendButton = QPushButton('send', self)
+        # Send Question to chat GPT
         self.sendButton.clicked.connect(self.sendToChatGPT)
         self.thinkLabel = QLabel("Listening ... ", self)
 
-
+        # Add widgets to grid
         self.layout.addWidget(self.infoSentence, 0, 0, 1, 2)
         self.layout.addWidget(self.chatBox, 1, 0, 2, 2)
         self.layout.addWidget(self.inputTxt, 4, 0)
@@ -46,52 +48,43 @@ class ClippyChat(QWidget):
         self.layout.addWidget(self.thinkLabel, 3, 0, 1, 2)
         self.setLayout(self.layout)
 
+    # Sends text from input to Chat GPT
     def sendToChatGPT(self):
-        print("clicked")
         question = self.inputTxt.text()
+        # Thread to allow gui to function while it queries chatGPT
         self.worker = Type(question)
         self.worker.gptResult.connect(self.complete)
         self.chatBox.appendHtml("<b>You: </b>" + question)
         self.inputTxt.clear()
+        # Disable buttons to user doesnt overwhelm chatGPT
         self.inputTxt.setDisabled(True)
         self.sendButton.setDisabled(True)
-        self.thinkLabel.setText("Thinking...")
+        self.thinkLabel.setText("Thinking ...")
         self.worker.start()
-        """""
-        if self.workerResponse.isRunning():
-            self.chatBox.appendHtml("<b>Please wait until I'm done.</b>")
 
-        else:
-            self.chatBox.appendHtml("<b>You: </b>" + question)
-            self.inputTxt.clear()
-            # calling open ai
-            self.workerResponse.start()
-        self.workerResponse.finished.connect(self.complete())
-
-        #self.workerResponse.start()
-        #self.worker
-
-        #self.talking = True
-        #self.worker = Speak(ai_response_msg)
-        #self.worker.start()
-        #self.worker.finished.connect(self.complete)
-        """
-
-    # Updated talking boolean so the talking animation halts
+    """
+    Callback function for worker thread thats sending and receiving data from chatgpt api
+    Re enables the buttons
+    Appends ChatGPT text to chatbox
+    Starts another thread for speaking
+    Set talking boolean for chatting animation
+    """ 
     def complete(self, ai_response_msg):
-        self.thinkLabel.setText("Listening...")
+        self.thinkLabel.setText("Talking ...")
         self.inputTxt.setDisabled(False)
         self.sendButton.setDisabled(False)
+        self.inputTxt.setFocus(True)
         self.chatBox.appendHtml("<b>Clippy: </b>" + ai_response_msg)
         self.workerSpeak = Speak(ai_response_msg)
         self.workerSpeak.finished.connect(self.doneTalking)
         self.workerSpeak.start()
         self.talking = True
-        print("done")
     
+    #Callback for speech worker
     def doneTalking(self):
+        self.thinkLabel.setText("Listening ...")
         self.talking = False
-
+#Thread for ChatGPT inquiries
 class Type(QThread):
     gptResult = QtCore.pyqtSignal(object)
     def __init__(self, text):
@@ -109,13 +102,12 @@ class Type(QThread):
         ai_response_msg = ai_response.choices[0].text
         print(ai_response_msg)
         self.gptResult.emit(ai_response_msg)
-        # self.chatBox.appendHtml("<b>Clippy: </b>" + ai_response_msg)
-
+#Thread for speaking
 class Speak(QThread):
     def __init__(self, speech):
         super(QThread, self).__init__()
         self.speech = speech
-
+    # Plays Audio received from gTTS
     def run(self):
         tts = gtts.gTTS(self.speech)
         tts.save("talk.mp3")
