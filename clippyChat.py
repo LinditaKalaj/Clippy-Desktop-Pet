3de22c98d5c433922d837baa0d1186709ad94b1d
lindita <linditakalaj@gmail.com>
2023-01-14 19:43:54 -0500

    Added mute button to stop clippy from talking in next propmpt

--- clippyChat.py
+++ clippyChat.py
@@ -3,7 +3,7 @@ import gtts as gtts
 import openai as openai
 import os
 
-from PyQt5 import QtCore, QtMultimedia
+from PyQt5 import QtCore, QtMultimedia, QtGui
 from dotenv import load_dotenv
 from PyQt5.QtCore import Qt, QThread, QFileInfo, QUrl
 from PyQt5.QtWidgets import *
@@ -28,7 +28,7 @@ class ClippyChat(QWidget):
         self.layout = QGridLayout()
         self.infoSentence = QLabel("Let's Have a chat!", self)
         self.infoSentence.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.infoSentence.setAlignment(Qt.AlignCenter)
+        self.infoSentence.setAlignment(Qt.AlignRight)
         self.chatBox = QPlainTextEdit(self)
         self.chatBox.setReadOnly(True)
         self.inputTxt = QLineEdit(self)
@@ -36,17 +36,32 @@ class ClippyChat(QWidget):
         self.inputTxt.returnPressed.connect(self.sendToChatGPT)
         self.inputTxt.setPlaceholderText("Ask a question!")
         self.sendButton = QPushButton('send', self)
+        # Mute Button
+        self.soundButton = QPushButton('', self)
+        self.soundButton.setStyleSheet("border-image: url(img/sound.png);")
+        self.soundButton.setFixedSize(30, 30)
+        self.soundButton.setCheckable(True)
+        self.soundButton.clicked.connect(self.changeSoundButton)
+
         # Send Question to chat GPT
         self.sendButton.clicked.connect(self.sendToChatGPT)
         self.thinkLabel = QLabel("Listening ... ", self)
 
         # Add widgets to grid
-        self.layout.addWidget(self.infoSentence, 0, 0, 1, 2)
+        self.layout.addWidget(self.soundButton, 0, 1, alignment=QtCore.Qt.AlignRight)
+        self.layout.addWidget(self.infoSentence, 0, 0)
         self.layout.addWidget(self.chatBox, 1, 0, 2, 2)
         self.layout.addWidget(self.inputTxt, 4, 0)
         self.layout.addWidget(self.sendButton, 4, 1)
         self.layout.addWidget(self.thinkLabel, 3, 0, 1, 2)
         self.setLayout(self.layout)
+
+    # Changes the sound button styling to match the state
+    def changeSoundButton(self):
+        if self.soundButton.isChecked():
+            self.soundButton.setStyleSheet("border-image: url(img/silent.png);")
+        else:
+            self.soundButton.setStyleSheet("border-image: url(img/sound.png);")
 
     # Sends text from input to Chat GPT
     def sendToChatGPT(self):
@@ -75,10 +90,13 @@ class ClippyChat(QWidget):
         self.sendButton.setDisabled(False)
         self.inputTxt.setFocus(True)
         self.chatBox.appendHtml("<b>Clippy: </b>" + ai_response_msg)
-        self.workerSpeak = Speak(ai_response_msg)
-        self.workerSpeak.talkingEnded.connect(self.doneTalking)
-        self.workerSpeak.start()
-        self.talking = True
+
+        # check if mute button is active
+        if not self.soundButton.isChecked():
+            self.workerSpeak = Speak(ai_response_msg)
+            self.workerSpeak.talkingEnded.connect(self.doneTalking)
+            self.workerSpeak.start()
+            self.talking = True
     
     #Callback for speech worker
     def doneTalking(self):
